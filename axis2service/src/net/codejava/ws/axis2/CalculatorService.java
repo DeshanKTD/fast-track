be90a40d72b8c5791e30d278bfa533fb198b90b7
DeshaKTD <deshankalupahana@gmail.com>
2017-11-16 09:59:23 +0530

    axis2 service server working

---
+++ axis2service/src/net/codejava/ws/axis2/CalculatorService.java
@@ -0,0 +1,12 @@
+package net.codejava.ws.axis2;
+ 
+/**
+ * A simple POJO class.
+ * @author www.codejava.net
+ */
+public class CalculatorService {
+ 
+    public int sum(int x, int y) {
+        return (x + y);
+    }
+}
